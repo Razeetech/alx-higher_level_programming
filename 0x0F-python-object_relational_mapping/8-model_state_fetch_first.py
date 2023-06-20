8de0c499c3922b284efb209b66b814aeb6e04aec
Razeetech <rajiazeezsch@gmail.com>
2023-06-19 21:20:33 -0700

    ALX 0x0F. Python - Object-relational mapping, Python, ORM, SQLAlchemy

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,32 +1,21 @@
 #!/usr/bin/python3
-"""script that prints the first State object from the database"""
-
-
+"""List all states"""
+from sys import argv
 from model_state import Base, State
-import sqlalchemy
-import sqlalchemy.orm
-import sys
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
 
-
-def main():
-    """List the names and IDs of U.S. states in a database"""
-
-    if len(sys.argv) < 4:
-        sys.exit(1)
-    engine = sqlalchemy.create_engine('mysql://{}:{}@localhost/{}'.format(
-        sys.argv[1],
-        sys.argv[2],
-        sys.argv[3]
-    ))
+if __name__ == "__main__":
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'
+        .format(argv[1], argv[2],
+                argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
-    Session = sqlalchemy.orm.sessionmaker(bind=engine)
+    Session = sessionmaker(bind=engine)
     session = Session()
-    record = session.query(State).order_by(State.id).first()
-    if record is None:
-        print('Nothing')
+    state = session.query(State).order_by(State.id).first()
+    if state:
+        print("{}: {}".format(state.id, state.name))
     else:
-        print(str(record.id) + ': ' + record.name)
-
-
-if __name__ == '__main__':
-    main()
+        print("Nothing")
+    session.close()
