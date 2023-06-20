fb1af69614cf9426781017224e61a5c5666ade82
Razeetech <rajiazeezsch@gmail.com>
2023-06-19 21:22:33 -0700

    ALX 0x0F. Python - Object-relational mapping, Python, ORM, SQLAlchemy

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,30 +1,19 @@
 #!/usr/bin/python3
-"""script that lists all State objects that contain the letter 'a' from the database"""
-
-
+"""List all states"""
+from sys import argv
 from model_state import Base, State
-import sqlalchemy
-import sqlalchemy.orm
-import sys
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
 
-
-def main():
-    """List the names and IDs of U.S. states in a database"""
-
-    if len(sys.argv) < 4:
-        sys.exit(1)
-    engine = sqlalchemy.create_engine('mysql://{}:{}@localhost/{}'.format(
-        sys.argv[1],
-        sys.argv[2],
-        sys.argv[3]
-    ))
+if __name__ == "__main__":
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'
+        .format(argv[1], argv[2],
+                argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
-    Session = sqlalchemy.orm.sessionmaker(bind=engine)
+    Session = sessionmaker(bind=engine)
     session = Session()
-    query = session.query(State).filter(State.name.like('%a%'))
-    if query.count() > 0:
-        print('\n'.join(str(state.id) + ': ' + state.name for state in query))
-
-
-if __name__ == '__main__':
-    main()
+    for state in session.query(State).\
+            filter(State.name.like('%a%')).order_by(State.id).all():
+        print("{}: {}".format(state.id, state.name))
+    session.close()
