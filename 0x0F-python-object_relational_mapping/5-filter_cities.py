b9c4e25983be7656c46d67a90bd50e23fbf874dc
Razeetech <rajiazeezsch@gmail.com>
2023-06-19 21:18:51 -0700

    ALX 0x0F. Python - Object-relational mapping, Python, ORM, SQLAlchemy

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,31 +1,19 @@
 #!/usr/bin/python3
-"""Module to list states in a database"""
-
+"""Lists states"""
 
 import MySQLdb
-import sys
+from sys import argv
 
-
-def main():
-    """Lists the names and IDs of U.S. states in a database"""
-
-    if len(sys.argv) < 5:
-        sys.exit(1)
-    connection = MySQLdb.connect(
-        host='localhost',
-        port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3]
-    )
-    cursor = connection.cursor()
-    cursor.execute('''
-        SELECT * FROM `states`
-        WHERE BINARY `name` = %s ORDER BY `id` ASC
-    ''', (sys.argv[4],))
-    records = cursor.fetchall()
-    if len(records) > 0:
-        print('\n'.join(str(record) for record in sorted(records)))
-
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+    conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                           passwd=argv[2], db=argv[3], charset="utf8")
+    cur = conn.cursor()
+    cur.execute("""
+        SELECT cities.name FROM cities
+        JOIN states ON cities.state_id = states.id
+        WHERE states.name = %s
+        ORDER BY cities.id ASC
+        """, (argv[4], ))
+    print(", ".join(map(lambda x: x[0], cur.fetchall())))
+    cur.close()
+    conn.close()
