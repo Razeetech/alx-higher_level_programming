10cc9a9a5ab9a033f56fd7621daf96caaba0d4b3
Razeetech <rajiazeezsch@gmail.com>
2023-06-19 21:36:58 -0700

    ALX 0x0F. Python - Object-relational mapping, Python, ORM, SQLAlchemy

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,20 +1,31 @@
 #!/usr/bin/python3
-"""List all states"""
-from sys import argv
-from model_state import Base, State
-from sqlalchemy import (create_engine)
-from sqlalchemy.orm import sessionmaker
+"""script that adds the State object 'Louisiana' to the database"""
 
-if __name__ == "__main__":
-    engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost/{}'
-        .format(argv[1], argv[2],
-                argv[3]), pool_pre_ping=True)
+
+from model_state import Base, State
+import sqlalchemy
+import sqlalchemy.orm
+import sys
+
+
+def main():
+    """List the names and IDs of U.S. states in a database"""
+
+    if len(sys.argv) < 4:
+        sys.exit(1)
+    engine = sqlalchemy.create_engine('mysql://{}:{}@localhost/{}'.format(
+        sys.argv[1],
+        sys.argv[2],
+        sys.argv[3]
+    ))
     Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
+    Session = sqlalchemy.orm.sessionmaker(bind=engine)
     session = Session()
-    new_state = State(name="Louisiana")
-    session.add(new_state)
+    new = State(name='Louisiana')
+    session.add(new)
     session.commit()
-    print(new_state.id)
-    session.close()
+    print(new.id)
+
+
+if __name__ == '__main__':
+    main()
