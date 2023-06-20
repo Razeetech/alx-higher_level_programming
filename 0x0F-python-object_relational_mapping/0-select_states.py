7f43cbf6e0e69f650af42c3e386739be1d548ae0
Razeetech <rajiazeezsch@gmail.com>
2023-06-19 21:03:58 -0700

    ALX 0x0F. Python - Object-relational mapping, Python, ORM, SQLAlchemy

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,29 +1,16 @@
 #!/usr/bin/python3
-"""Script that lists all states in a database"""
-
+"""Lists states"""
 
 import MySQLdb
-import sys
+from sys import argv
 
-
-def main():
-    """Lists the names and IDs of U.S. states in a database"""
-
-    connection = MySQLdb.connect(
-        host='localhost',
-        port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3]
-    )
-    cursor = connection.cursor()
-    cursor.execute('SELECT * FROM `states` ORDER BY `id` ASC;')
-    records = cursor.fetchall()
-    if len(records) > 0:
-        print('\n'.join(str(record) for record in records))
-    cursor.close()
-    connection.close()
-
-
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+    conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                           passwd=argv[2], db=argv[3], charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
